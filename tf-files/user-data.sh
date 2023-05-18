0150eff63a1ef19f4fcf5dcbe01ac99244423bc0
srivishal1 <vishal.srivastava@3pillarglobal.com>
2023-05-18 12:37:58 +0530

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -6,6 +6,5 @@ yum install python3 -y
 pip3 install flask
 pip3 install flask_mysql
 yum install git -y
-TOKEN="*************************"
-cd /home/ec2-user && git clone https://$TOKEN@github.com/devenes/terraform-alb-asg-app.git
+cd /home/ec2-user && git clone https://github.com/srivishal1/terraform-alb-asg-app.git
 python3 /home/ec2-user/terraform-alb-asg-app/phonebook-app.py
