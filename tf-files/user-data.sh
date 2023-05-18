fe66ae1fa825575195225cec7152a276a69224cb
srivishal1 <vishal.srivastava@3pillarglobal.com>
2023-05-18 16:12:51 +0530

    Update user-data.sh

--- tf-files/user-data.sh
+++ tf-files/user-data.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-
-yum update -y
-yum install python3 -y
-pip3 install flask
-pip3 install flask_mysql
-yum install git -y
-cd /home/ec2-user && git clone https://github.com/srivishal1/terraform-alb-asg-app.git
-python3 /home/ec2-user/terraform-alb-asg-app/phonebook-app.py
+sudo yum update -y
+sudo yum install python3 -y
+sudo pip3 install flask
+sudo pip3 install flask_mysql
+sudo yum install git -y
+sudo cd /home/ec2-user && git clone https://github.com/srivishal1/terraform-alb-asg-app.git
+sudo python3 /home/ec2-user/terraform-alb-asg-app/phonebook-app.py
